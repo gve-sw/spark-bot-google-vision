761e87be91b9cce22b5af62c2b75465554a1ec02
Hantzley Tauckoor <hantzley@gmail.com>
2017-10-29 11:48:49 +1100

    Bot

--- app.py
+++ app.py
@@ -600,7 +600,7 @@ def listener():
 
             elif 'help' in message['text']:
                 post_message_to_room(spark_headers,roomID,"Hey! This is bot is easy to use. \
-                    Just post an image to the Spark room, and it will tell you what it sees. \
+                    Just post an image to the Spark room, and the Bot will tell you what it sees. \
                     Give it a try and send me your feedback.\n\nThank you,\n\n" + signature)
 
     return "OK"
