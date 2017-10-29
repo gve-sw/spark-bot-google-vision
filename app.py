6d3cdbb3eb7c9523d861cafec093cd95f7b73e19
Hantzley Tauckoor <hantzley@gmail.com>
2017-10-29 12:13:13 +1100

    Links

--- app.py
+++ app.py
@@ -46,7 +46,10 @@ filename = None
 base_img_width=1024
 
 signature = "Hantzley Tauckoor - [\[Email\]](mailto:htauckoo@cisco.com) \
-    [\[LinkedIn\]](http://linkedin.com/in/hantzley)"
+    [\[LinkedIn\]](http://linkedin.com/in/hantzley) \
+    [\[@hantzley\]](http://twitter.com/hantzley) \
+    [\[GitHub\]](http://github.com/hantzley)"
+
 project_info = "Liked this Bot? Want to contribute or create your own? See \
     the project on [GitHub](https://github.com/Hantzley/spark-bot-google-vision)"
 
